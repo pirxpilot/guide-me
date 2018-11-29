a3a5f1968035f348e0e33ef3e0796e3faea37f32
Damian Krzeminski <pirxpilot@furkot.com>
2018-11-28 19:18:45 -0700

    rewrite in ES6

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var overlay = require('overlay');
-var Popover = require('confirmation-popover');
-var Emitter = require('emitter');
-
-module.exports = Tour;
+const overlay = require('overlay');
+const Popover = require('confirmation-popover');
+const Emitter = require('emitter');
 
 function id2el(id) {
-  return document.querySelector('[data-tour-id="' + id + '"]') || document.querySelector(id);
+  return document.querySelector(`[data-tour-id="${id}"]`) || document.querySelector(id);
 }
 
 function coerce(selectorOrNode) {
@@ -13,7 +11,7 @@ function coerce(selectorOrNode) {
     // node or empty
     return selectorOrNode;
   }
-  var el = document.querySelector(selectorOrNode);
+  const el = document.querySelector(selectorOrNode);
   if (el.nodeName === 'TEMPLATE' && el.content) {
     return el.content;
   }
@@ -23,9 +21,9 @@ function coerce(selectorOrNode) {
 // parse HTML to create a list of steps - tour-id / tour-content need to match
 function steps(container) {
   return Array.prototype.map.call(container.querySelectorAll('[data-tour-content]'), function(el) {
-    var id = el.dataset.tourContent;
+    const id = el.dataset.tourContent;
     return {
-      id: id,
+      id,
       contentEl: el,
       position: el.dataset.position || 'bottom',
       delay: el.dataset.delay || 0,
@@ -40,7 +38,7 @@ function steps(container) {
 }
 
 function createPopover(step) {
-  var self = this;
+  const self = this;
   if (step.absent) {
     step.refEl = id2el(step.id);
     self.markStep(true);
@@ -53,129 +51,127 @@ function createPopover(step) {
     .cancel(self.labels.cancel)
     .ok(self.labels.ok)
     .focus('ok')
-    .on('show', function() {
-      self.emit('show', self.current);
-    })
-    .on('hide', function() {
-      self.emit('hide', self.current);
-    })
-    .on('cancel', self.end.bind(self))
-    .on('ok', self.next.bind(self))
+    .on('show', () => self.emit('show', self.current))
+    .on('hide', () => self.emit('hide', self.current))
+    .on('cancel', () => self.end())
+    .on('ok', () => self.next())
     .position(step.position)
     .show(step.refEl);
 }
 
-function Tour(container, options) {
-  if (!(this instanceof Tour)) return new Tour(container, options);
-  this.steps = steps(coerce(container));
-  this.current = 0;
-  this.labels = Object.assign({
-    ok: 'Next',
-    cancel: 'Close'
-  }, options && options.labels);
+class Tour extends Emitter {
+  static of(...args) {
+    return new Tour(...args);
+  }
+
+  constructor(container, { labels } = {}) {
+    super();
+    this.steps = steps(coerce(container));
+    this.current = 0;
+    this.labels = Object.assign({
+      ok: 'Next',
+      cancel: 'Close'
+    }, labels);
+  }
+
+  overlay(options) {
+    this._overlay = overlay(options);
+    this._overlay.el.classList.add('tour-overlay');
+    return this;
+  }
+
+  play(index) {
+    const self = this;
+
+    self.emit('begin');
+    if (self._overlay) {
+      self._overlay.show();
+    }
+    if (typeof index === 'number') {
+      self.current = index;
+    }
+    self.showStep();
+  }
+
+  // hides next button for last step
+  updateNext() {
+    this.popover.el.querySelector('.ok').classList.toggle('hidden', this.current + 1 >= this.steps.length);
+  }
+
+  // marks element associated with active step
+  markStep(on) {
+    const step = this.steps[this.current];
+    if (step) {
+      step.refEl.classList.toggle('tour-active-step', on);
+    }
+  }
+
+  hideStep() {
+    if (this.popover) {
+      this.popover.hide();
+      this.popover = undefined;
+    }
+  }
+
+  showStep() {
+    let step;
+
+    this.current %= this.steps.length;
+    step = this.steps[this.current];
+
+    if (!step) {
+      return;
+    }
+    if (!step.absent) {
+        this.markStep(true);
+    }
+
+    this.hideStep();
+
+    setTimeout(createPopover.bind(this, step), step.delay);
+  }
+
+  // called when user acted upon a suggestion in a Tour step
+  react(delay) {
+    const step = this.steps[this.current];
+
+    if (!step) {
+      return;
+    }
+    if (!this.popover) {
+      return;
+    }
+    if (this.popover.el.classList.contains('tour-reacted')) {
+      return;
+    }
+
+    if (typeof delay !== 'number') {
+      delay = step.delay;
+    }
+
+    const popover = this.popover.hide();
+    setTimeout(function() {
+      popover.show(step.refEl);
+      popover.classname += ' tour-reacted';
+      popover.el.classList.add('tour-reacted');
+    }, delay);
+  }
+
+  next() {
+    this.markStep(false);
+    this.emit('next', ++this.current);
+    setTimeout(() => this.showStep(), 0);
+  }
+
+  end() {
+    this.markStep(false);
+    if (this._overlay) {
+      this._overlay.hide();
+    }
+    this.hideStep();
+    ++this.current;
+    this.emit('end');
+  }
 }
 
-Emitter(Tour.prototype);
-
-Tour.prototype.overlay = function(options) {
-  this._overlay = overlay(options);
-  this._overlay.el.classList.add('tour-overlay');
-  return this;
-};
-
-Tour.prototype.play = function(index) {
-  var self = this;
-
-  self.emit('begin');
-  if (self._overlay) {
-    self._overlay.show();
-  }
-  if (typeof index === 'number') {
-    self.current = index;
-  }
-  self.showStep();
-};
-
-// hides next button for last step
-Tour.prototype.updateNext = function() {
-  this.popover.el.querySelector('.ok').classList.toggle('hidden', this.current + 1 >= this.steps.length);
-};
-
-// marks element associated with active step
-Tour.prototype.markStep = function(on) {
-  var step = this.steps[this.current];
-  if (step) {
-    step.refEl.classList.toggle('tour-active-step', on);
-  }
-};
-
-Tour.prototype.hideStep = function() {
-  if (this.popover) {
-    this.popover.hide();
-    this.popover = undefined;
-  }
-};
-
-Tour.prototype.showStep = function() {
-  var step;
-
-  this.current %= this.steps.length;
-  step = this.steps[this.current];
-
-  if (!step) {
-    return;
-  }
-  if (!step.absent) {
-      this.markStep(true);
-  }
-
-  this.hideStep();
-
-  setTimeout(createPopover.bind(this, step), step.delay);
-};
-
-// called when user acted upon a suggestion in a Tour step
-Tour.prototype.react = function(delay) {
-  var step = this.steps[this.current];
-
-  if (!step) {
-    return;
-  }
-  if (!this.popover) {
-    return;
-  }
-  if (this.popover.el.classList.contains('tour-reacted')) {
-    return;
-  }
-
-  if (typeof delay !== 'number') {
-    delay = step.delay;
-  }
-
-  var popover = this.popover.hide();
-  setTimeout(function() {
-    popover.show(step.refEl);
-    popover.classname += ' tour-reacted';
-    popover.el.classList.add('tour-reacted');
-  }, delay);
-};
-
-Tour.prototype.next = function() {
-  var self = this;
-
-  self.markStep(false);
-  self.emit('next', ++self.current);
-  setTimeout(self.showStep.bind(self), 0);
-};
-
-Tour.prototype.end = function() {
-  var self = this;
-
-  self.markStep(false);
-  if (self._overlay) {
-    self._overlay.hide();
-  }
-  self.hideStep();
-  ++self.current;
-  self.emit('end');
-};
+module.exports = Tour;
